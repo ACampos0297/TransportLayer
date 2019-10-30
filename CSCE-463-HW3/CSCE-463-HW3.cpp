4f6df58eba3f904fe6217f2715c47b9515fba157
ACampos0297 <abdulcampos02@gmail.com>
2019-10-29 23:16:39 -0500

    Create buffer and initialize socket. Added other class files.

--- CSCE-463-HW3/CSCE-463-HW3.cpp
+++ CSCE-463-HW3/CSCE-463-HW3.cpp
@@ -1,7 +1,66 @@
+/*
+	Abdul Campos
+	325001471
+	HW2 P1
+	CSCE 463 - 500
+	Fall 2019
+*/
 
-#include <iostream>
+#include "pch.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	if (argc < 8) //wrong command line arguments
+	{
+		printf("usage: [destination server] [buffer size] [sender window]"
+			"[RTT propagation delay] [forward loss rate] "
+			"[return loss rate] [link speed]\n");
+		return -1;
+	}
+
+	string server = argv[1]; //destination server
+	int bufferSize, senderWindow, linkSpeed; //buffer size (power of 2) Sender window in packets, link speed
+	float RTT, fwLossRate, rtLossRate; //RTT in seconds, loss rates
+
+	bufferSize = atoi(argv[2]);
+	senderWindow = atoi(argv[3]);
+	linkSpeed = atoi(argv[7]);
+
+	//check if power of 2
+	if (!(bufferSize && (!(bufferSize & (bufferSize - 1)))))
+	{
+		printf("usage: [destination server] [buffer size] [sender window]"
+			"[RTT propagation delay] [forward loss rate] "
+			"[return loss rate] [link speed]\n");
+		return -1;
+	}
+
+	RTT = strtof(argv[4],NULL);
+	fwLossRate = strtof(argv[5], NULL);
+	rtLossRate = strtof(argv[6], NULL);
+
+	//Main information
+	printf("Main:\tsender W = %d, RTT %.3f sec, loss %.0e / %.0e, link %d Mbps\n",
+		senderWindow, RTT, fwLossRate, rtLossRate, linkSpeed);
+	printf("Main:\tInitializing DWORD array with 2^%d elements... ", bufferSize);
+
+	//start clock
+	clock_t tlast = clock();
+
+	//initialize buffer
+	UINT64  dwordBufSize = (UINT64) 1 << bufferSize;
+	DWORD* dwordBuf = new DWORD[dwordBufSize]; // user-requested buffer
+	for (UINT64 i = 0; i < dwordBufSize; i++) // required initialization
+		dwordBuf[i] = i;
+	
+	printf("done in %d ms\n", (clock()-tlast)/CLOCKS_PER_SEC);
+
+	//Initialize WinSock; once per program run
+	WSADATA wsaData;
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	if (WSAStartup(wVersionRequested, &wsaData) != 0) {
+		printf("WSAStartup error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
 }
---
+++ CSCE-463-HW3/SenderSocket.cpp
@@ -0,0 +1,1 @@
+#include "SenderSocket.h"
