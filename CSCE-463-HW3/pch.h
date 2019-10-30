32c4e472394b6aaf8dd91ab6f18bf9ae6cf04e39
ACampos0297 <abdulcampos02@gmail.com>
2019-10-29 23:32:34 -0500

    Added header structs.

--- CSCE-463-HW3/pch.h
+++ CSCE-463-HW3/pch.h
@@ -28,5 +28,25 @@
 #include <ctime>
 #include <fstream>
 
+// SenderSocket.h
+#define MAGIC_PORT 22345 // receiver listens on this port
+#define MAX_PKT_SIZE (1500-28) // maximum UDP packet size accepted by receiver
+
+//sender socket errors
+#define STATUS_OK 0 // no error
+#define ALREADY_CONNECTED 1 // second call to ss.Open() without closing connection
+#define NOT_CONNECTED 2 // call to ss.Send()/Close() without ss.Open()
+#define INVALID_NAME 3 // ss.Open() with targetHost that has no DNS entry
+#define FAILED_SEND 4 // sendto() failed in kernel
+#define TIMEOUT 5 // timeout after all retx attempts are exhausted
+#define FAILED_RECV 6 // recvfrom() failed in kernel
+
+//Flag header
+#define MAGIC_PROTOCOL 0x8311AA 
+
+//LinkProperties
+#define FORWARD_PATH 0
+#define RETURN_PATH 1 
+
 using namespace std;
 #endif //PCH_H
